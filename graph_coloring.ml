fe0ba589c37fe914a8e07c20125f6c4dd4c964ef
Radouanos <redwan.b6@gmail.com>
2020-04-27 18:02:04 +0200

    Revert "the function Color (Qt 12) and to_dot2"
    This reverts commit 12f804d368399c97d9f2a2c8bb43965eb05b0541.

--- graph_coloring.ml
+++ graph_coloring.ml
@@ -6,70 +6,51 @@ module StringMap = Map.Make(String)
 
 type graph = StringSet.t StringMap.t
 
-(*to_dot
-  Modification de to_dot
-*)
-    
+
+
+(*to_dot*)
 let to_dot g =
   let _ = Printf.printf "digraph MonGraph {\n" in
   let _ =
-    StringMap.iter (fun u us -> if(StringSet.is_empty us)
-      then
-	Printf.printf " %s -> Empty" u
-      else 
-	StringSet.iter (fun v -> Printf.printf "  %s -> %s;\n" u v) us) g
+    StringMap.iter (fun u us -> StringSet.iter (fun v -> Printf.printf "  %s -> %s;\n" u v) us) g
   in
   Printf.printf "}\n"
 
 (*fin to_dot*)
 
 (*add_edge*)
-    
 let add_edge_aux u v g = 
-  let u_succ = try StringMap.find u g with Not_found -> StringSet.empty in  (* Get successor nodes of u *)
-  let u_succ' = (StringSet.add v u_succ) in  (* add v to the successor of u *)
+  let u_succ = try StringMap.find u g with Not_found -> StringSet.empty in   
+  let u_succ' = (StringSet.add v u_succ) in
   (StringMap.add u u_succ' g)     
-
-         (* We used add_edge_aux to help as to add both nodes as successors ...*) 
-    
+  
 let add_edge u v g =
-  let g' = (add_edge_aux u v g) in (* add v as a successor of u *)
-  (add_edge_aux v u g') (* add also u as a successor of u *)
-
+  let g' = (add_edge_aux u v g) in
+  (add_edge_aux v u g')   
+  
 let graph =
   let g = add_edge "a" "b" StringMap.empty in
-  let g = add_edge "b" "c" g in
-  add_edge "c" "a" g 
-
+  let g = add_edge "a" "e" g in
+  add_edge "g" "m" g     
 
 (*fin add_edge*)
 
-    
 (*remove_vertex*)
-    
 let remove_edge u v g = 
   let u_succ = try StringMap.find u g with Not_found -> StringSet.empty in   
-  let u_succ' = (StringSet.remove v u_succ) in (StringMap.add u u_succ' g)
+  let u_succ' = (StringSet.remove v u_succ) in
+  (StringMap.add u u_succ' g)
 
 
 let remove_vertex u g =
   let voisins_de_u = try StringMap.find u g with Not_found -> StringSet.empty in
-  let g' = StringMap.remove u g in StringSet.fold (fun v a -> remove_edge v u a) voisins_de_u g'
-       
-(*
-digraph MonGraph {
-  a -> b;
-  a -> c;
-  a -> d;
-  b -> a;
-  c -> a;
-  d -> a;
-}
-
-remove_vertex "a" graph5 ;;
-
-if faut qu'on suprimme aussi les noeuds liés au noeud 
-fin remove_vertex*)
+  let g' = StringMap.remove u g in
+      StringSet.fold
+        (fun v a ->
+          remove_edge v u a)
+        voisins_de_u
+        g'
+(*fin remove_vertex*)
 
 
 (*Coloriages disponibles*)
@@ -84,28 +65,19 @@ module IntSet = Set.Make(Int) (*On crée un module IntSet qui aura les mêmes at
 
 
 (*color_set*)
-  
 let rec color_set_aux i j acc = if i > j then IntSet.empty 
                                 else if i = j then IntSet.add i acc 
                                 else let acc1 = IntSet.add i acc in (color_set_aux (i+1) j acc1)
 
-let color_set j =  color_set_aux 1 j IntSet.empty
-
-(* 
-   should we verify if we use this exception or not ?
-   if j < 1 then failwith  "the color's number should be bigger than 0"
-*)
-    
+let color_set j = color_set_aux 1 j IntSet.empty
 (*fin color_set*)
 
 (*disp_color*)
-    
-type disp_color = IntSet.t StringMap.t (* i think that we should use StringSet and IntSet TODO *) 
-    
+type disp_color = IntSet.t StringMap.t
 (*fin disp_color*)
 
-(*to_dot_init_colors*)
 
+(*to_dot_init_colors*)
 let to_dot_init_colors g =
   let _ = Printf.printf "MonGraph init colors {\n" in
   let _ =
@@ -116,12 +88,10 @@ let to_dot_init_colors g =
 (*fin to_dot_init_colors*)
 
 (*add_edge_init_colors*)
-    
 let add_edge_init_colors u v g = 
- (fun x -> if x < 5 then raise (Failed "Echec") else x) let u_succ = try StringMap.find u g with Not_found -> IntSet.empty in   
+  let u_succ = try StringMap.find u g with Not_found -> IntSet.empty in   
   let u_succ' = (IntSet.add v u_succ) in
   (StringMap.add u u_succ' g)
-
 (*fin add_edge_init_colors*)
 
 
@@ -131,31 +101,41 @@ let init_colors g k =
     let nodes_of_g = StringMap.fold (fun u us a -> StringSet.add u a) g acc in
       let range = color_set k in
         let graph = StringMap.empty in
-        StringSet.fold (fun v g -> IntSet.fold (fun r g1-> add_edge_init_colors v r g1) range g) nodes_of_g graph
-	  
+          StringSet.fold
+            (fun v g -> 
+              IntSet.fold
+                (fun r g1->
+                add_edge_init_colors v r g1)
+                range
+                g
+            )
+            nodes_of_g
+            graph
 (*fin init_colors*)
 
 (*remove_color*)
-	    
 let remove_color i v c =
-  let v_succ = StringMap.find v c  in   (* est ce qu'il faut pas laisser l'exception ?? *)
-    let v_succ' = (IntSet.remove i v_succ) in (StringMap.add v v_succ' c)
-      
+  let v_succ = try StringMap.find v c with Not_found -> IntSet.empty in   
+    let v_succ' = (IntSet.remove i v_succ) in
+      (StringMap.add v v_succ' c)
 (*fin remove_color*)
 
 (*exception Failed*)
-      
 exception Failed of string
-   
+
+(*Test*)
+let test_exception f = if f=0 then raise (Failed "Pas de chance! Aucun coloriage n'a ete trouve.")
+(*fin test*)
 (*fin exception Failed*)
 
 (*try_first*)
-    
 let rec try_first f s = 
   let exception_message = "Pas de chance! Aucun coloriage n'a ete trouve." in
     if s = IntSet.empty then raise (Failed exception_message)
     else let i = IntSet.choose s in
-      try f i with Failed _ -> let s' = IntSet.remove i s in try_first f s'
+      try f i 
+      with Failed _ -> let s' = IntSet.remove i s in
+                        try_first f s'
 
 (*Test*)
 let s =                             
@@ -167,42 +147,10 @@ let s =
 let try_first_test = try_first (fun x -> if x < 4 then raise (Failed "Echec") else x) s
 
 let try_first_test = try_first (fun x -> if x < 5 then raise (Failed "Echec") else x) s
-  
 (*fin test*)
 (*fin try_first*)
 
 
 (*coloring*)
-  
-type coloring = int StringMap.t ;; (* je propose int StringSet.t *)
-
+type coloring = int StringMap.t;;
 (*fin coloring*)
-
-
-
-(* 
-   color function
-*)
-let choosefrom graph=
-  let (noeud,voisins) = StringMap.choose graph in noeud
-					       
-
-let rec color g c =
-  if StringMap.is_empty g then StringMap.empty
-  else
-    let (s,voisins) = StringMap.choose g in
-
-    let s_colors = StringMap.find s c in
-
-    try_first (fun col -> let g' = remove_vertex s g in
-			  let c' = StringSet.fold (fun v gc -> remove_color col v gc) voisins  c in
-			  StringMap.add s col (color g' c')   
-      )
-      s_colors
-
-let to_dot_init_colors_2 g =
-  let _ = Printf.printf "MonGraph init colors {\n" in
-  let _ =
-    StringMap.iter (fun u us -> Printf.printf "  %s -> %d;\n" u us) g
-  in
-  Printf.printf "}\n"
